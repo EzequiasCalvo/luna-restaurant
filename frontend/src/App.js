aac0f1075d1661e9515974fcbfe72693db9b4b74
zequi <calvo.ezequias@gmail.com>
2021-09-20 11:15:57 +0200

    refactor routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,25 +4,16 @@ import Login from "./authentication/login";
 import Registration from "./authentication/registration";
 import Message from "./authentication/registration/message";
 import Verification from "./authentication/registration/verification";
+import Home from "./home";
 
 function App() {
   return (
     <Router>
-      <Route path="/" exact>
-        LUNA HOME
-      </Route>
-      <Route path="/login">
-        <Login />
-      </Route>
-      <Route path="/signup">
-        <Registration />
-      </Route>
-      <Route path="/message">
-        <Message />
-      </Route>
-      <Route path="/verification">
-        <Verification />
-      </Route>
+      <Route path="/" exact component={Home}></Route>
+      <Route path="/login" exact component={Login}></Route>
+      <Route path="/signup" exact component={Registration}></Route>
+      <Route path="/message" exact component={Message}></Route>
+      <Route path="/verification" exact component={Verification}></Route>
     </Router>
   );
 }
